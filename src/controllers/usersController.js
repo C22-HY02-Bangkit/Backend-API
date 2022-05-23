27fc54ada12b5fb0317e5cee200a110d7d1c71e0
Rachmi <iamrachbutwithmi@gmail.com>
2022-05-24 06:47:53 +0700

    Auth Login

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,10 @@
-const { genSaltSync, hashSync } = require('bcryptjs');
+const { genSaltSync, hashSync, compareSync } = require('bcryptjs');
 const User = require('../models').user;
-const { validationResult } = require('express-validator');
+const { validationResult, body } = require('express-validator');
 const { errorMsgTrans } = require('../utils/transform');
 const { v4: uuidv4 } = require('uuid');
 const { generateToken } = require('../utils/tokenManager');
+const bcrypt = require('bcryptjs');
 
 // note: cuman untuk contoh - akan dihapus dikemudian hari
 exports.me = async (req, res) => {
@@ -64,11 +65,48 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
+    const bodyData = req.body;
+
+    // check body
+    for (const key in bodyData) {
+        const payload = ['email', 'password'];
+
+        // check updateable data
+        if (!payload.includes(key)) {
+            res.status(400).json({
+                code: 400,
+                message: 'Invalid! Please check your input!',
+                error: `${key} is not valid!`,
+            });
+        }
+    }
+
+    // validate body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(403).json({
+            code: 403,
+            message: 'Invalid input!',
+            errors: errorMsgTrans(errors.array()),
+        });
+    }
+
+    // hash password
+    const user = await User.findOne({ where: { email: bodyData.email } });
+
+    const passwordMatch = compareSync(bodyData.password, user.password);
+
+    if (!passwordMatch) {
+        res.status(401).json({
+            code: 401,
+            message: 'Invalid Password!',
+        });
+    }
 
     res.json({
         code: 200,
         status: 'success',
-        message: 'Login success',
+        message: 'Login success!',
+        token: generateToken(user.id),
     });
 };
