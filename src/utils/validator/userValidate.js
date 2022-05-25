c80c4bed8b27863d0437a6a098a892eb976dc1e3
alfi sahri <alfisahri33@gmail.com>
2022-05-25 19:29:54 +0700

    add file - refactor user validate

---
+++ src/utils/validator/userValidate.js
@@ -0,0 +1,54 @@
+const { check } = require('express-validator');
+
+const registerValidate = [
+    check('fullname').notEmpty().withMessage('fullname is required'),
+    check('email')
+        .notEmpty()
+        .withMessage('email is required')
+        .isEmail()
+        .custom((value) => {
+            return User.findOne({ where: { email: value } }).then((data) => {
+                if (data) {
+                    return Promise.reject('email already exists!');
+                }
+            });
+        }),
+    check('password').notEmpty().withMessage('password is required'),
+];
+
+const loginValidate = [
+    check('email').notEmpty().withMessage('email is required').isEmail(),
+    check('password').notEmpty().withMessage('password is required'),
+];
+
+const forgotPasswordValidate = [
+    check('email').notEmpty().withMessage('email is required').isEmail(),
+];
+
+const resetPasswordValidate = [
+    check('user_id')
+        .notEmpty()
+        .isUUID()
+        .withMessage('user_id format must be UUID'),
+    check('password').notEmpty(),
+];
+
+const verifyEmailValidate = [
+    check('user_id')
+        .notEmpty()
+        .isUUID()
+        .withMessage('user_id format must be UUID'),
+];
+
+const resendEmailValidate = [
+    check('email').notEmpty().withMessage('email is required').isEmail(),
+];
+
+module.exports = {
+    registerValidate,
+    loginValidate,
+    forgotPasswordValidate,
+    resetPasswordValidate,
+    verifyEmailValidate,
+    resendEmailValidate,
+};
