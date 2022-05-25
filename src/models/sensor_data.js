1c1d34864744fe019fe7bf9e3cc327d7436116d9
alfi sahri <alfisahri33@gmail.com>
2022-05-25 08:13:54 +0700

    add association

--- src/models/sensor_data.js
+++ src/models/sensor_data.js
@@ -2,13 +2,11 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class sensor_data extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
-            // define association here
+            device.belongsTo(models.device, {
+                foreignKey: 'device_id',
+                as: 'device',
+            });
         }
     }
     sensor_data.init(
