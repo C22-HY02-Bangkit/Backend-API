8682359631f7e7e0cf719010c83599c5fa72357e
alfi sahri <alfisahri33@gmail.com>
2022-05-29 21:08:41 +0700

    add realtime server with socket.io

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+require('express-async-errors');
+
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
-const { Sequelize } = require('sequelize');
+const sql = require('./config/db');
 const ErrorHandler = require('./utils/errorHandler');
-
-require('dotenv').config();
-require('express-async-errors');
-
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    },
+});
+
 const port = process.env.PORT || 5000;
 const host = process.env.HOST || 'localhost';
 
@@ -23,6 +29,14 @@ app.use(xss());
 
 // routes
 app.use('/api', require('./routes'));
+
+// realtime server
+io.on('connection', (socket) => {
+    sql.listen('new_data', (payload) => {
+        const data = JSON.parse(payload);
+        io.emit('realtime', data.new);
+    });
+});
 
 // 404 endpoint
 app.all('*', (req, res) => {
