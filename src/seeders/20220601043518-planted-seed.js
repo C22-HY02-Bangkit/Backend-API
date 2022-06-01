242b0f58f62f8ffe7dd73b75ef0560cdf5125472
alfi sahri <alfisahri33@gmail.com>
2022-06-01 11:44:55 +0700

    adding plants and planted seeder

---
+++ src/seeders/20220601043518-planted-seed.js
@@ -0,0 +1,28 @@
+'use strict';
+const Device = require('../models').device;
+const Plant = require('../models').plant;
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        const plants = await Plant.findAll();
+        const devices = await Device.findAll();
+
+        return queryInterface.bulkInsert('planted', [
+            {
+                id: uuidv4(),
+                plant_id: plants[0].id,
+                device_id: devices[0].id,
+                ph: 12,
+                tds: 12,
+                ec: 12,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+            },
+        ]);
+    },
+
+    async down(queryInterface, Sequelize) {
+        return queryInterface.bulkDelete('planted', null, {});
+    },
+};
