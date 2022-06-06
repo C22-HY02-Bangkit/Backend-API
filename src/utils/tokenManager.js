b25cdff2291dcae4c0e64d1bcba263049bfcb93d
alfi sahri <alfisahri33@gmail.com>
2022-06-06 14:17:36 +0700

    change token expires

--- src/utils/tokenManager.js
+++ src/utils/tokenManager.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-exports.generateToken = (id) => jwt.sign({ id }, process.env.SECRET_KEY, {expiresIn: '1d'});
+exports.generateToken = (id) => jwt.sign({ id }, process.env.SECRET_KEY, {expiresIn: '30d'}); 
 
 exports.verifyToken = (token) => jwt.verify(token, process.env.SECRET_KEY);
