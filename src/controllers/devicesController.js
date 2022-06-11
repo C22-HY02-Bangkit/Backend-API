0ff77d3651d3b2dc7b934e84cdd0c3e3f5b2522a
alfi sahri <alfisahri33@gmail.com>
2022-06-11 10:29:55 +0700

    update user device response data

--- src/controllers/devicesController.js
+++ src/controllers/devicesController.js
@@ -2,6 +2,7 @@ const AppError = require('../utils/AppError');
 const { validationResult, matchedData } = require('express-validator');
 const { errorMsgTrans } = require('../utils/transform');
 const Device = require('../models').device;
+const Plant = require('../models').plant;
 const { v4: uuidv4 } = require('uuid');
 
 exports.getDevices = async (req, res) => {
@@ -10,7 +11,8 @@ exports.getDevices = async (req, res) => {
     // get all device belong to user
     const devices = await Device.findAll({
         where: { user_id },
-        include: ['planted'],
+        attributes: ['id', 'code', 'name'],
+        include: [{ model: Plant, as: 'planted', attributes: ['id', 'name'] }],
     });
 
     // check if exists
@@ -30,6 +32,8 @@ exports.getDevice = async (req, res) => {
     //get device detail by id
     const deviceDetail = await Device.findOne({
         where: { id },
+        attributes: ['id', 'code', 'name', 'user_id'],
+        include: [{ model: Plant, as: 'planted', attributes: ['id', 'name'] }],
     });
 
     if (!deviceDetail)
@@ -44,42 +48,6 @@ exports.getDevice = async (req, res) => {
         code: 200,
         status: 'success',
         data: deviceDetail,
-    });
-};
-
-exports.addDevice = async (req, res) => {
-    const { id: user_id } = req.user;
-
-    // validate body
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const msg = errorMsgTrans(errors.array({ onlyFirstError: true }));
-        throw new AppError(msg, 400);
-    }
-
-    // get body data based on validator
-    const bodyData = matchedData(req, { locations: ['body'] });
-
-    //add new device
-    const newDevice = await Device.create({
-        id: uuidv4(),
-        user_id: user_id,
-        name: bodyData.name,
-        code: bodyData.code,
-    });
-
-    //check new device
-    if (!newDevice) throw new AppError('Add device failed!', 400);
-
-    res.json({
-        code: 200,
-        status: 'success',
-        message: 'Device added!',
-        data: {
-            id: newDevice.id,
-            name: newDevice.name,
-            code: newDevice.code,
-        },
     });
 };
 
@@ -119,37 +87,6 @@ exports.editDevice = async (req, res) => {
         code: 200,
         status: 'success',
         message: 'Update device success!',
-        data: {
-            id: device.id,
-        },
-    });
-};
-
-exports.deleteDevice = async (req, res) => {
-    const { id } = req.params;
-    const { id: user_id } = req.user;
-
-    //find device
-    const device = await Device.findOne({ where: { id } });
-    if (!device) throw new AppError('Device not found!', 404);
-
-    // check if user has access
-    if (device.user_id !== user_id) {
-        throw new AppError('Access forbidden!', 403);
-    }
-
-    //delete device
-    const deleteDevice = await device.destroy();
-
-    //check delete process
-    if (!deleteDevice) throw new AppError('Delete device failed!', 400);
-
-    res.json({
-        code: 200,
-        status: 'success',
-        message: 'Delete device success!',
-        data: {
-            id: device.id,
-        },
+        device_id: device.id,
     });
 };
